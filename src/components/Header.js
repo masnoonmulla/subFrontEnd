da4ce129ebf523f6fbfc4725bd6a61e450b0c527
Ashok Naik <naikashok08@gmail.com>
2022-04-07 17:25:27 +0400

    Resolved Issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,81 @@
-import React from 'react';
-import  "./Header.css";
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <div className="desktopHeaderNav">
-    <nav className="navBarMain">
-      <div className="navBarLeft">
-        <div className="navBrandName">
-          <p className="navtext">Subjective</p>
-        </div>
-      </div>
-      <div className="navSearchBar">
-        <div className="navInrSearchBar">
-          {/* This div is for Search List Box navSearchResults */}
-          <div>
-            <div className="navInrSearchInput">
-              <div className="navSearchIcon">
-                {/* Search Icon Goes Here */}
-              </div>
-              <input
-                type="text"
-                className="navSearchInput"
-                placeholder="Search Sneakers,Designers...."
-              ></input>
-            </div>
-          </div>
+    <header className="header">
+      <a href="" className="logo">
+        Subjective
+      </a>
+
+      <div className="searchBarNew">
+        <div className="navInrSearchInput">
+          <div className="navSearchIcon"></div>
+          <input
+            type="text"
+            className="navSearchInput"
+            placeholder="Search Sneakers,Designers...."
+          ></input>
         </div>
       </div>
 
-      <ul className="menuList">
-        <div style={{display:'flex'}}>
-          <li className="menuListItem">
-            <p className="listItemText">Favorites</p>
-          </li>
-          <li className="menuListItem">
-            <p className="listItemText">Orders</p>
-          </li>
-          <li className="menuListItem">
-            <p className="listItemText">Login</p>
-          </li>
-        </div>
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon" htmlFor="menu-btn">
+        <span className="navicon"></span>
+      </label>
+      <ul className="menu">
+        <li>
+          <a href="#work"> Favourites </a>
+        </li>
+        <li>
+          <a href="#about">Orders</a>
+        </li>
+        <li>
+          <a href="#careers">Login</a>
+        </li>
       </ul>
-    </nav>
-  </div>
-  )
+    </header>
+    //   <div className="desktopHeaderNav">
+    //   <nav className="navBarMain">
+    //     <div className="navBarLeft">
+    //       <div className="navBrandName">
+    //         <p className="navtext">Subjective</p>
+    //       </div>
+    //     </div>
+    //     <div className="navSearchBar">
+    //       <div className="navInrSearchBar">
+    //        
+    //         <div>
+    //           <div className="navInrSearchInput">
+    //             <div className="navSearchIcon">
+    //               {/* Search Icon Goes Here */}
+    //             </div>
+    //             <input
+    //               type="text"
+    //               className="navSearchInput"
+    //               placeholder="Search Sneakers,Designers...."
+    //             ></input>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //     <ul className="menuList">
+    //       <div style={{display:'flex'}}>
+    //         <li className="menuListItem">
+    //           <p className="listItemText">Favorites</p>
+    //         </li>
+    //         <li className="menuListItem">
+    //           <p className="listItemText">Orders</p>
+    //         </li>
+    //         <li className="menuListItem">
+    //           <p className="listItemText">Login</p>
+    //         </li>
+    //       </div>
+    //     </ul>
+    //   </nav>
+    // </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/screens/HomePage/index.js
+++ src/screens/HomePage/index.js
@@ -1,57 +1,56 @@
 import React from "react";
 import "./home-page.css";
 import Header from "../../components/Header";
+import Carousel from "react-grid-carousel";
 
 function HomePage() {
   return (
     <>
       <Header />
-      <div className="firstContainerDesc">
-        <div className="firstInnerContainerDesc">
-          <div className="firstMainContainerDesc">
-            <div className="mainBlockContainer">
-              <div className="firstImgContainer">
-                <div className="firstImgCard">
-                  <div className="imgDiv">
-                    <img
-                      src={require("../../assets/Artboard.png")}
-                      className="imgStyle"
-                    ></img>
-                  </div>
-                </div>
+
+      <div className="columnsNew">
+        <div className="left-div left-text">
+          <div className="firstImgCard">
+            <div className="imgDiv">
+              <img
+                src={require("../../assets/Artboard.png")}
+                className="imgStyle"
+              ></img>
+            </div>
+          </div>
+        </div>
+
+        <div className="right-div right-text">
+          <div className="firstDescContainer">
+            <h1 className="firstDescTitle">Mission Statment</h1>
+
+            <span className="subHeaderTitle">
+            Art is a medium of expression for us. We at Subjective believe in being true to ourselves and embodying our own uniqueness. Wear who you are!
+            </span>
+
+            <div className="buttonContainer">
+              <div className="firstButtonCon">
+                <a className="buttonWh">Login As Customer</a>
               </div>
 
-              <div className="firstDescContainer">
-                <h1 className="firstDescTitle">Mission Statment</h1>
-
-                <span className="subHeaderTitle">
-                  Letraset sheets containing Lorem Ipsum passages, and more
-                  recently with desktop publishing software like Aldus PageMaker
-                  including versions of Lorem Ipsum.
-                </span>
-
-                <div className="buttonContainer">
-                  <div className="firstButtonCon">
-                    <a className="buttonWh">Login As Customer</a>
-                  </div>
-
-                  <div className="firstButtonCon">
-                    <a className="buttonNew">Login As Artist</a>
-                  </div>
-                </div>
+              <div className="firstButtonCon">
+                <a className="buttonNew">Login As Artist</a>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       <div className="secondSection">
         <h2 className="howItWorksText"> How it Works ?</h2>
         <div className="howItWorksContainer">
-
           <div className="hwcard">
             <div className="hwContent">
-              <div className="hwAvatar"> 
-                <img className="imgStyle"   src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png" />
+              <div className="hwAvatar">
+                <img
+                  className="avaStyle"
+                  src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png"
+                />
               </div>
               It was popularised in the 1960s with the release of Letraset
               sheets containing Lorem Ipsum passages, and more recently with
@@ -62,8 +61,11 @@ function HomePage() {
 
           <div className="hwcard">
             <div className="hwContent">
-              <div className="hwAvatar"> 
-                <img className="imgStyle"   src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png" />
+              <div className="hwAvatar">
+                <img
+                  className="avaStyle"
+                  src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png"
+                />
               </div>
               It was popularised in the 1960s with the release of Letraset
               sheets containing Lorem Ipsum passages, and more recently with
@@ -74,8 +76,205 @@ function HomePage() {
 
           <div className="hwcard">
             <div className="hwContent">
-              <div className="hwAvatar"> 
-                <img className="imgStyle"   src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png" />
+              <div className="hwAvatar">
+                <img
+                  className="avaStyle"
+                  src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png"
+                />
+              </div>
+              It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with
+              desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="thirdSection">
+        <h2 className="howItWorksText"> Customization Categories </h2>
+        <div className="howItWorksContainer">
+          <div className="catGroup">
+            <img
+              className="avaStyle"
+              src={require("../../assets/sneakersCat.jpg")}
+            />
+
+            <p className="categoryText"> Sneakers</p>
+          </div>
+
+          <div className="catGroup">
+            <img
+              className="avaStyle"
+              src={require("../../assets/t-shirt.jpg")}
+            />
+
+            <p className="categoryText">Clothing</p>
+          </div>
+
+          <div className="catGroup">
+            <img
+              className="avaStyle"
+              src={require("../../assets/bagcat.jpg")}
+            />
+
+            <p className="categoryText">Bag</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="fouthSection">
+        <h2 className="featuredArtistText"> Featured Artist </h2>
+
+        <Carousel cols={3} rows={1} gap={10} loop>
+          <Carousel.Item>
+            <div className="card">
+              <div className="header">
+                <div className="avatar">
+                  <img
+                    src="https://randomuser.me/api/portraits/women/67.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="card-body">
+                <div className="user-meta has-text-centered">
+                  <h3 className="username">Helen Miller</h3>
+                  <h5 className="position">Sneaker,Bag Customiser</h5>
+                </div>
+                <div className="user-bio has-text-centered">
+                  <p>
+                    {" "}
+                    Letraset sheets containing Lorem Ipsum passages, and more
+                    recently with desktop publishing software like Aldus
+                    PageMaker including versions of Lorem Ipsum.
+                  </p>
+
+                  <p className="sugButton"> #fanart </p>
+                  <p className="sugButton"> #fanart </p>
+                  <p className="sugButton"> #fanart </p>
+                </div>
+
+                <div className="ratingRow">
+                  <div className="ratingCol">
+                    <img
+                      src={require("../../assets/strImg.png")}
+                      style={{ width: 120 }}
+                      alt=""
+                    />
+                    <p> Rating </p>
+                  </div>
+                  <div className="ratingCol">
+                    <p> Price Range: 5k-10K </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="card">
+              <div className="header">
+                <div className="avatar">
+                  <img
+                    src="https://randomuser.me/api/portraits/women/67.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="card-body">
+                <div className="user-meta has-text-centered">
+                  <h3 className="username">Helen Miller</h3>
+                  <h5 className="position">Sneaker,Bag Customiser</h5>
+                </div>
+                <div className="user-bio has-text-centered">
+                  <p>
+                    {" "}
+                    Letraset sheets containing Lorem Ipsum passages, and more
+                    recently with desktop publishing software like Aldus
+                    PageMaker including versions of Lorem Ipsum.
+                  </p>
+
+                  <p className="sugButton"> #fanart </p>
+                  <p className="sugButton"> #fanart </p>
+                  <p className="sugButton"> #fanart </p>
+
+                 
+                </div>
+                <div className="ratingRow">
+                    <div className="ratingCol">
+                      <img
+                        src={require("../../assets/strImg.png")}
+                        style={{ width: 120 }}
+                        alt=""
+                      />
+                      <p> Rating </p>
+                    </div>
+                    <div className="ratingCol">
+                      <p> Price Range: 5k-10K </p>
+                    </div>
+                  </div>
+
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="card">
+              <div className="header">
+                <div className="avatar">
+                  <img
+                    src="https://randomuser.me/api/portraits/women/67.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="card-body">
+                <div className="user-meta has-text-centered">
+                  <h3 className="username">Helen Miller</h3>
+                  <h5 className="position">Sneaker,Bag Customiser</h5>
+                </div>
+                <div className="user-bio has-text-centered">
+                  <p>
+                    {" "}
+                    Letraset sheets containing Lorem Ipsum passages, and more
+                    recently with desktop publishing software like Aldus
+                    PageMaker including versions of Lorem Ipsum.
+                  </p>
+
+                  <p className="sugButton"> #fanart </p>
+                  <p className="sugButton"> #fanart </p>
+                  <p className="sugButton"> #fanart </p>
+                </div>
+
+
+                <div className="ratingRow">
+                  <div className="ratingCol">
+                    <img
+                      src={require("../../assets/strImg.png")}
+                      style={{ width: 120 }}
+                      alt=""
+                    />
+                    <p> Rating </p>
+                  </div>
+                  <div className="ratingCol">
+                    <p> Price Range: 5k-10K </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+
+      <div className="fifthSection">
+        <h2 className="howItWorksText"> How to sell your Art</h2>
+        <div className="howItWorksContainer">
+          <div className="hwcard">
+            <div className="hwContent">
+              <div className="hwAvatar">
+                <img
+                  className="avaStyle"
+                  src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png"
+                />
               </div>
               It was popularised in the 1960s with the release of Letraset
               sheets containing Lorem Ipsum passages, and more recently with
@@ -84,7 +283,50 @@ function HomePage() {
             </div>
           </div>
 
-        
+          <div className="hwcard">
+            <div className="hwContent">
+              <div className="hwAvatar">
+                <img
+                  className="avaStyle"
+                  src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png"
+                />
+              </div>
+              It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with
+              desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </div>
+          </div>
+
+          <div className="hwcard">
+            <div className="hwContent">
+              <div className="hwAvatar">
+                <img
+                  className="avaStyle"
+                  src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png"
+                />
+              </div>
+              It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with
+              desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </div>
+          </div>
+          
+          {/* <div className="hwcard">
+            <div className="hwContent">
+              <div className="hwAvatar">
+                <img
+                  className="avaStyle"
+                  src="https://www.playtoearn.online/wp-content/uploads/2021/10/Bored-Ape-Yacht-Club-NFT-avatar.png"
+                />
+              </div>
+              It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with
+              desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </div>
+          </div> */}
 
 
         </div>
