a5489fe1d4649337d2ce5fd9a7c5bb5c1b201345
masnoonmulla <masnoonmulla09@gmail.com>
2022-05-05 19:49:35 +0530

    05-05-22: Login & Dashboard Design

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import HomePage from './screens/HomePage';
 import CategoryScreen from './screens/CategoryScreen';
 import ArtistDashboard from './screens/ArtistDashboard';
-import { BrowserRouter,  Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './screens/Auth/Login';
+import Dashboard from './components/Dashboard';
 
 
 function App() {
@@ -11,16 +13,16 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <div>
-          
-            <Routes>
-             <Route path="/" element={<HomePage/>} exact/>
-             <Route path="/discover" element={<CategoryScreen/>}/>
-             <Route path="/artist-dashboard" element={<ArtistDashboard/>}/>
-            <Route component={Error}/>
-           </Routes>
-        </div> 
+          <Routes>
+            {/* <Route path="/" element={<HomePage />} exact /> */}
+            <Route path="/" element={<Dashboard />} exact />
+            <Route path="/login" element={<Login />} exact />
+            <Route path="/discover" element={<CategoryScreen />} />
+            <Route path="/artist-dashboard" element={<ArtistDashboard />} />
+            <Route component={Error} />
+          </Routes>
+        </div>
       </BrowserRouter>
-    
     </div>
   );
 }
